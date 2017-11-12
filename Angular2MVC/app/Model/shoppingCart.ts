9e00bd6a261388fc9dec705e65d453f435ca9421
giangc5467 <33336399+giangc5467@users.noreply.github.com>
2017-11-12 17:20:39 -0600

    Add files via upload

---
+++ Angular2MVC/app/Model/shoppingCart.ts
@@ -0,0 +1,13 @@
+﻿//import { IProduct } from '../Model/product';
+//import { IUser } from '../Model/user';
+import { userItem } from '../Model/userItem';
+
+
+export class shoppingCart {
+    Id: number;
+ //   productId: number;
+ //   productList: IProduct[];
+    userId: number;
+    userItemList: userItem[];
+  //  userList: IUser[];
+}
---
+++ Angular2MVC/app/Model/userItem.ts
@@ -0,0 +1,8 @@
+﻿import { IUser } from '../Model/user';
+
+
+export class userItem {
+    Quantity: number;
+    Subtotal: number; //only need quant and subt in product page.
+    user: IUser;
+}
